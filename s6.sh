62076cfd5a412f6b06d355f932be697c9b7601eb
Adrià Sabaté De la iglesia <15583899.clot@fje.edu>
2022-04-01 19:07:52 +0200

    m06uf2pr2

--- s6.sh
+++ s6.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+echo "Introdueix el directori d'origen: "
+read dir1
+
+tar -cvf $dir1.tar $dir1
+
+echo "Introdueix el destí de l'arxiu: "
+read dir2
+
+cp -rf $dir1.tar $dir2
+
+exit 1
