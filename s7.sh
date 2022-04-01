bf6cdba6977221ed3921b127d026c90dff48310f
Adrià Sabaté De la iglesia <15583899.clot@fje.edu>
2022-04-01 16:29:11 +0200

    Pràctica m06uf2pr2

--- s7.sh
+++ s7.sh
@@ -1,39 +1,32 @@
 #!/bin/bash
-echo "# Script 7 per Martí Rodríguez" > VagrantFile
+echo "# Vagrant file fet per Adrià Sabaté" > VagrantFile
 echo "Vagrant.configure("'"'"2"'"'") do |config|" >> VagrantFile
-echo  "config.vm.box = "'"'"generic/debian10"'"'"" >> VagrantFile
-echo "config.vm.hostname = "'"'"maroma"'"'"" >> VagrantFile
-echo  "config.vm.provider "'"'"virtualbox"'"'" do |v|"  >> VagrantFile
-    # v.gui = true
-echo  "v.name = "'"'"Script7Maroma"'"'"" >> VagrantFile
-echo    "v.memory = 2048" >> VagrantFile
-echo    "v.cpus = 1"  >> VagrantFile
-echo    "v.customize ['modifyvm', :id, '--clipboard', 'bidirectional']"  >> VagrantFile
-echo  "end"  >> VagrantFile
-
-echo  "config.vm.network "'"'"forwarded_port"'"'", guest: 80, host: 8000"  >> VagrantFile
-echo  "config.vm.network "'"'"forwarded_port"'"'", guest: 443, host: 8080" >> VagrantFile
-echo  "config.vm.network "'"'"forwarded_port"'"'", guest: 3306, host: 8888"  >> VagrantFile
-echo  "config.vm.network "'"'"forwarded_port"'"'", guest: 22, host: 8808"  >> VagrantFile
-
-echo  "config.vm.provision "'"'"shell"'"'", inline: <<-SHELL"  >> VagrantFile
-echo    "sudo apt-get update -y"  >> VagrantFile
-echo    "sudo apt-get upgrade -y" >> VagrantFile
-echo    "sudo apt-get install -y net-tools" >> VagrantFile
-echo    "sudo apt-get install -y openssh" >> VagrantFile
-echo    "sudo apt-get install -y apache2 apache2-doc" >> VagrantFile
-echo "Vols instalar els paquets extra? [s per si, n per No]"
+echo "config.vm.box = "'"'"generic/debian10"'"'"" >> VagrantFile
+echo "config.vm.hostname = "'"'"adsade"'"'"" >> VagrantFile
+echo "config.vm.provider "'"'"virtualbox"'"'" do |v|"  >> VagrantFile
+echo "v.name = "'"'"sh7adsade"'"'"" >> VagrantFile
+echo "v.memory = 2048" >> VagrantFile
+echo "v.cpus = 1"  >> VagrantFile
+echo "v.customize ['modifyvm', :id, '--clipboard', 'bidirectional']"  >> VagrantFile
+echo "end"  >> VagrantFile
+echo "config.vm.network "'"'"forwarded_port"'"'", guest: 80, host: 8000"  >> VagrantFile
+echo "config.vm.network "'"'"forwarded_port"'"'", guest: 443, host: 8080" >> VagrantFile
+echo "config.vm.network "'"'"forwarded_port"'"'", guest: 3306, host: 8888"  >> VagrantFile
+echo "config.vm.network "'"'"forwarded_port"'"'", guest: 22, host: 8808"  >> VagrantFile
+echo "config.vm.provision "'"'"shell"'"'", inline: <<-SHELL"  >> VagrantFile
+echo "sudo apt-get update -y"  >> VagrantFile
+echo "sudo apt-get upgrade -y" >> VagrantFile
+echo "sudo apt-get install -y net-tools" >> VagrantFile
+echo "sudo apt-get install -y openssh" >> VagrantFile
+echo "sudo apt-get install -y apache2 apache2-doc" >> VagrantFile
+echo "Vols instalar els paquets extra? [S per si, N per no]"
 read opc
 if  [[  $opc  ==  "s" ]]
 then
-echo    "sudo apt-get install -y openssh-sftp-server" >> VagrantFile
-echo    "sudo apt-get install -y mariadb-server" >> VagrantFile
-echo    "sudo apt-get install -y mariadb-client" >> VagrantFile
+echo "sudo apt-get install -y openssh-sftp-server" >> VagrantFile
+echo "sudo apt-get install -y mariadb-server" >> VagrantFile
+echo "sudo apt-get install -y mariadb-client" >> VagrantFile
 fi
-
 echo  "SHELL" >> VagrantFile
-
 echo "end"  >> VagrantFile
-
-
 exit 0
