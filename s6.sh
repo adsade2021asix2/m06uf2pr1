7ebe727e5ac56954e2a6df084d40bfe6ef621a6c
Adrià Sabaté De la iglesia <15583899.clot@fje.edu>
2022-04-03 18:45:43 +0200

    Pràctica m06uf2pr2

--- s6.sh
+++ s6.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+local fitxer
+
 echo "Introdueix el directori d'origen: "
 read dir1
 
-tar -cvf $dir1.tar $dir1
+for fitxer in $1/*
+do
+	if [[ -s $fitxer ]] && [[ -x $fitxer ]]
+	then
+		tar -cvf $dir1.tar $dir1
+	fi
+done
+#tar -cvf $dir1.tar $dir1
 
 echo "Introdueix el destí de l'arxiu: "
 read dir2
